bc2ecc24e561deaf997361905cdb2a2b44ae3298
Karen Shumate <kshumate@bellsouth.net>
2019-05-27 10:12:15 -0400

    add js for resume

---
+++ resume.js
@@ -0,0 +1,12 @@
+if (connection != null){
+    MediaKeySystemAccess.out.println("Connection to database successful");
+}
+
+<a href="KarenShumateResumeHTML6-1-19.docx"></a>;
+
+els (connection) {
+    MediaKeySystemAccess.out.println("Unable to connect to database.");
+}
+
+
+
